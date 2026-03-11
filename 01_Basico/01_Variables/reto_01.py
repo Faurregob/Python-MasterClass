7a3e6a0362d2d70d890857e411db2f53bf2bd845
faurrego <analistadatos2@vise.com.co>
2026-03-11 08:53:20 -0500

    Creación del Repositorio para el aprendizaje Autonomo de Python

---
+++ 01_Basico/01_Variables/reto_01.py
@@ -0,0 +1,8 @@
+nombre = "Freddy Alexander Urrego Beltran"
+edad = 36
+altura = 1.75
+desarrollador = True
+print(f"Mi nombre es: {nombre}.\n"
+      f"Mi edad es: {edad} años.\n"
+      f"Mi estatura es: {altura} metros.\n"
+      f"¿Soy desarrollador? {desarrollador}.")
