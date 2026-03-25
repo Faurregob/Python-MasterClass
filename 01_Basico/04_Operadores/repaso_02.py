5c14ac6cc5fcc61695375bf27168645f851b35c8
faurrego <analistadatos2@vise.com.co>
2026-03-25 16:28:20 -0500

    Seguimos avanzando con el curso apoyado con IA para el tema de condicionales

---
+++ 01_Basico/04_Operadores/repaso_02.py
@@ -0,0 +1,10 @@
+celsius = float(input("Por favor ingrese la temperatura en °C: "))
+
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+print(f"Grados Celsius: {celsius}°C.\n"
+      f"Grados Fahrenheit: {fahrenheit}°F\n"
+      f"Grados Kelvin: {kelvin}°K")
+
+
